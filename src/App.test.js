b80e61e6d5abbfdfcceedd281cb3943f4ef2f1f7
B6137151 <59865985+B6137151@users.noreply.github.com>
2024-05-25 03:39:00 +0700

    Update Jest and Babel configuration for frontend CI/CD

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // for toBeInTheDocument matcher
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByRole('heading', { name: /login/i });
+  expect(headerElement).toBeInTheDocument();
 });
